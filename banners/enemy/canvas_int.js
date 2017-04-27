7726a37a0c72889c338a8eb0573a7589bd7adcc7
Kela <fosh.design@gmail.com>
2017-04-27 11:52:42 +0300

    sc_enemy

---
+++ banners/enemy/canvas_int.js
@@ -0,0 +1,42 @@
+var canvas, stage, exportRoot;
+
+
+
+function init() {
+
+
+//SCRIPT START//////////////////////////////////////////////////////////////
+
+canvas = document.getElementById("canvas");
+	images = images||{};
+
+	var loader = new createjs.LoadQueue(false);
+	loader.addEventListener("fileload", handleFileLoad);
+	loader.addEventListener("complete", handleComplete);
+	loader.loadManifest(lib.properties.manifest);
+}
+
+function handleFileLoad(evt) {
+	if (evt.item.type == "image") { images[evt.item.id] = evt.result; }
+}
+
+function handleComplete(evt) {
+	exportRoot = new lib.stop_the_enemy();
+
+	stage = new createjs.Stage(canvas);
+	stage.addChild(exportRoot);
+	stage.update();
+
+	createjs.Ticker.setFPS(lib.properties.fps);
+	createjs.Ticker.addEventListener("tick", stage);
+
+
+//SCRIPT END//////////////////////////////////////////////////////////////
+
+	stage.canvas.addEventListener("click", fl_ClickToGoToWebPage);
+
+    function fl_ClickToGoToWebPage() {
+    window.open("http://clickable.design/en/", "_blank");
+    }
+
+}
